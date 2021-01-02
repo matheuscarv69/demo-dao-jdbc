bb7a682670e846e7dd907ffd0880ffe065379dc3
matheuscarv69 <matheus9126@gmail.com>
2021-01-02 16:58:36 -0400

    dep insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -27,10 +27,10 @@ public class Program2 {
         List<Department> list = depDao.findAll();
         list.forEach(System.out::println);
 
-//        System.out.println("\n=== TEST 4 : seller insert ===");
-//        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-//        sellerDao.insert(newSeller);
-//        System.out.println("Inserted! New id = " + newSeller.getId());
+        System.out.println("\n=== TEST 3 : Department insert ===");
+        Department newDep = new Department(null, "Música");
+        depDao.insert(newDep);
+        System.out.println("Inserted! New id = " + newDep.getId());
 //
 //        System.out.println("\n=== TEST 5 : seller update ===");
 //        seller = sellerDao.findById(1);
