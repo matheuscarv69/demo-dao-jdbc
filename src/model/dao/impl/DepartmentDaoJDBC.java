4b5781c59799ed05728d4bb4705195101fd996dc
matheuscarv69 <matheus9126@gmail.com>
2021-01-02 17:11:32 -0400

    dep delete implementation

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -77,6 +77,26 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "DELETE FROM department "
+                            + "WHERE Id = ?"
+            );
+
+            st.setInt(1, id);
+
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new DbException("Error while deleting! Invalid Id");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
 
     }
 
